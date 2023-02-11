fe3e63c286df70643c9210f09b11f11959629cd4
Lucas <lucasmartinsms.dev@gmail.com>
2023-02-11 18:48:42 -0300

    customers

--- src/middlewares/customersMiddlewares.js
+++ src/middlewares/customersMiddlewares.js
@@ -3,7 +3,7 @@ import { connectionDB } from "../database/database.js";
 
 export async function customerSchemaValidation(req, res, next){
     const customer = req.body;
-    const { name, phone, cpf, birthday} = customer;
+
     
 
 
@@ -14,7 +14,7 @@ export async function customerSchemaValidation(req, res, next){
             const errors = error.details.map((detail) => detail.message);
             return res.status(400).send(errors);
         }
-        const customerExists = await connectionDB.query(`SELECT cpf FROM customers WHERE name = $1;`,[cpf]);
+        const customerExists = await connectionDB.query(`SELECT cpf FROM customers WHERE name = $1;`,[customer.cpf]);
         if(customerExists.rowCount > 0){
             return res.status(409).send("Este cliente já existe");
         }
