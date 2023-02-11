49b908eabea9a35040378de405d536038532ebab
Lucas <lucasmartinsms.dev@gmail.com>
2023-02-11 16:06:43 -0300

    gamesController

---
+++ src/controllers/gamesControllers.js
@@ -0,0 +1,31 @@
+import { connectionDB } from "../database/database.js";
+
+export async function getGames(req,res){
+    const findGame = req.query.name;
+    
+    try{
+        
+            if(findGame){
+                const search = await connectionDB.query(`SELECT * FROM games WHERE name ILIKE ($1);`,[`${findGame}%`]);
+                return res.status(200).send(search.rows);
+            }
+        const games = await connectionDB.query(`SELECT * FROM games`)
+        return res.status(200).send(games.rows);
+    }catch(err){
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
+
+export async function postGames(req,res){
+    const game = res.locals.game;
+    const {name, image, stockTotal, pricePerDay} = game;
+
+    try{
+        const games = await connectionDB.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`,[name,image,stockTotal,pricePerDay]);  
+        return res.sendStatus(201);
+    }catch(err){
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
