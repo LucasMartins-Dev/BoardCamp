731cdb1d3e7f8321c148dc07ed0eca01e467904e
Lucas <lucasmartinsms.dev@gmail.com>
2023-02-13 13:38:56 -0300

    fic

--- src/schemas/rentalsSchema.js
+++ src/schemas/rentalsSchema.js
@@ -3,10 +3,6 @@ import Joi from "joi";
 
 export const rentalSchema = Joi.object({
   customerId: Joi.number().integer().required(),
-  gameId: Joi.number().integer().required(),
-  rentDate: Joi.date(),    
-  daysRented: Joi.number(),             
-  returnDate: Joi.date().allow(null),         
-  originalPrice: Joi.number(),       
-  delayFee: Joi.number().allow(null),
+  gameId: Joi.number().integer().required(),   
+  daysRented: Joi.number()          
 })
